92033650328e570f1ee5d499059836006ec0bc05
benjamin <ruehl@lotum.de>
2017-10-24 14:28:55 +0200

    Rebuild library with changed PIXI imports

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import PIXI from'pixi.js';
+import * as PIXI from'pixi.js';
 import utils from './utils';
 import AudioManager from './AudioManager';
 import {audioParser, audioUrlParser} from './audioParser';
